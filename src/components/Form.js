37ee580cb478bf02b89ca3db8a25ac8fdd0a2f17
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-08-20 12:35:57 +0530

    many thing updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,17 +9,35 @@ export default function Form(props) {
         setText(newText);
     }
 
+    const handleLoClick = () => {
+        let newText = text.toLocaleLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
 
     return (
-        <div>
-            <div className="mb-3">
-                <h1>{props.heading}</h1>
-                <textarea className="form-control" value={text} id="box" rows="8" onChange={handleOnChange}></textarea>
+        <>
+            <div className="container">
+                <div className="mb-3">
+                    <h1>{props.heading}</h1>
+                    <textarea className="form-control" value={text} id="box" rows="8" onChange={handleOnChange}></textarea>
+                </div>
+                <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to upper case</button>
+                <button className="btn btn-primary" onClick={handleLoClick}>Convert to lower case</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to upper case</button>
-        </div>
+
+            <div className="container my-2">
+                <h2>You text summary</h2>
+                <p>{text.length} characters and {text.split(" ").length} words</p>
+                <p>{0.008 * text.split(" ").length} word will take minutes to read for an average person</p>
+
+                {/* preview */}
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
