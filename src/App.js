96cb68661ee07cfc351b69d185b352445ccd1597
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-08-20 17:38:12 +0530

    dark mode little bit improved

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import About from './components/About';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [theme, settheme] = useState('light')
+
+  const toggleMode = () => {
+    if(theme === 'light') {
+        settheme('dark');
+        document.body.style.background = 'grey'
+    } else if(theme === 'dark') {
+        settheme('light')
+        document.body.style.background = 'white'
+    }
+  }
+
   return (
     <>
-      <Navbar title="React-App" nav1="Home" nav2="About Us" />
+      <Navbar title="React-App" nav1="Home" nav2="About Us" mode={theme} toggleMode={toggleMode}/>
 
       <div className="container my-3">
         <Form heading="Enter the text to analyze" />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,33 +3,38 @@ import PropTypes from 'prop-types'
 
 
 export default function Navbar(props) {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">{props.nav1}</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">{props.nav2}</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">{props.title}</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">{props.nav1}</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">{props.nav2}</a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <div className="form-check form-switch">
+              <input className="form-check-input" type="checkbox" onClick={props.toggleMode} id="flexSwitchCheckDefault" />
+              <label className={`form-check-label text-${props.mode==="light"?"dark":"light"}`} htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+            </div>
+
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 
 Navbar.prototype = {
-    title: PropTypes.string.isRequired,
-    nav1: PropTypes.string
+  title: PropTypes.string.isRequired,
+  nav1: PropTypes.string
 }
