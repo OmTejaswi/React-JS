823ae4389ba5ff58dd169ce23086e8d07a6a96b3
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-08-22 13:36:59 +0530

    react router added and bugs fixed look improved

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import About from './components/About';
 import Alert from './components/Alert'
 import React, { useState } from 'react';
 
+// react router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
 
   const [theme, settheme] = useState('light');
   const [alert, setalert] = useState(null);
 
-  const showAlert = (type,message) => {
-        setalert({
-          typ: type, 
-          msg: message,
-        })
+  const showAlert = (type, message) => {
+    setalert({
+      typ: type,
+      msg: message,
+    })
 
-        setTimeout(() => {
-            setalert(null);
-        }, 2000);
+    setTimeout(() => {
+      setalert(null);
+    }, 2000);
   }
 
 
@@ -26,22 +34,34 @@ function App() {
     if (theme === 'light') {
       settheme('dark');
       document.body.style.background = '#042743'
-      showAlert("success",": Dark mode has been enabled");
+      showAlert("success", ": Dark mode has been enabled");
     } else if (theme === 'dark') {
       settheme('light')
       document.body.style.background = 'white'
-      showAlert("success",": Dark mode has been disabled");
+      showAlert("success", ": Dark mode has been disabled");
     }
   }
 
   return (
     <>
-      <Navbar title="React-App" nav1="Home" nav2="About Us" mode={theme} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Form heading="Enter the text to analyze" mode={theme} alert={showAlert}/>
-      </div>
-      <About mode={theme} />
+      <Router>
+        <Navbar title="TextUtils" nav1="Home" nav2="About Us" mode={theme} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+
+          <Switch>
+            <Route exact path="/about">
+              <About mode={theme} />
+            </Route>
+            <Route exact path="/home">
+              <Form heading="Enter the text to analyze" mode={theme} alert={showAlert} />
+            </Route>
+            <Route exact path="/React-JS">
+              <Form heading="Enter the text to analyze" mode={theme} alert={showAlert} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <br /><br />
     </>
   );
